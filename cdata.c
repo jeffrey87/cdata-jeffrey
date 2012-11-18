3bad37786059dcede6dc23ae5747e3f30e9b0840
Jeffrey <jeffrey@jeffrey-laptop.(none)>
2012-11-18 16:19:15 +0800

    exe3

--- cdata.c
+++ cdata.c
@@ -6,6 +6,7 @@
 #include <linux/miscdevice.h>
 #include <linux/wait.h>
 #include <asm/io.h>
+#include <asm/uaccess.h>
 
 #include "cdata_ioctl.h"
 
@@ -14,14 +15,30 @@
 #endif
 
 #define	CDATA_MAJOR 121 
+#define BUFSIZE 1024
+
+struct cdata_t {
+	char data[BUFSIZE];
+	int index;
+};
 
 static int cdata_open(struct inode *inode, struct file *filp)
 {
+	/* no reentrancy  because P1 and P2 have their own malloc*/ 
+	
 	int minor;
 
 	minor = MINOR(inode->i_rdev);
+	/*
 	printk(KERN_ALERT "cdata: in cdata_open(minor = %d)\n", minor);
 	printk(KERN_ALERT "cdata: the file struct address *filp is %d\n", filp);
+	*/
+	struct cdata_t *cdata;
+
+	cdata = (struct cdata_t *)kmalloc(sizeof(struct cdata_t), GFP_KERNEL);
+	cdata->index = 0;  /* the kmalloc does not clear the memory vlaue */ 
+	filp->private_data = (void *)cdata;
+
 	/*
 	while (1) {
 	}
@@ -37,12 +54,20 @@ static int cdata_ioctl(struct inode *inode, struct file *filp,
 
 	switch(cmd) 
 	{
+		int i;
+		struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
 		case CDATA_EMPTY:
 			printk(KERN_ALERT "in ioctl: IOCTL_EMPTY\n");
+			for (i = 0; i < BUFSIZE; i++)
+				cdata->data[i] = '';				
+			
 			break;
 
 		case CDATA_SYNC:
 			printk(KERN_ALERT "in inctl: IOCTL_SYNC\n");
+			for (i = 0; i < BUFSIZE; i++)
+				printk(KERN_ALERT "%c",cdata->data[i]);
+			
 			break;
 
 		default:
@@ -58,9 +83,31 @@ static ssize_t cdata_read(struct file *filp, char *buf,
 }
 
 static ssize_t cdata_write(struct file *filp, const char *buf, 
-				size_t size, loff_t *off)
+				size_t count, loff_t *off)
 {
+	
+	/* may have reentrant if open before fork */
+
 	printk(KERN_ALERT "cdata_write: %s\n", buf);
+
+	struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
+	int i;
+
+	/* if CPU is single it may not reentrant, if SMP, it may */
+
+	/* mutex_lock */
+	for (i = 0; i < count; i++) 
+	{
+		if (cdata->index >= BUFSIZE)
+			return -EFAULT;
+		
+		/* if function have let process sleep like kmalloc,vmalloc, copy_from_user...  it must reentrant whather single or SMP */
+		/* synchronization (share data) problem */
+
+		if (copy_from_user(&cdata->data[cdata->index++], &buf[i],1))
+			return -EFAULT;
+	}	
+	/* mutex_unlock */
 	return 0;
 }
 
